bcff50ca6ebfdb060001487c926508d3e020a1c9
gbeznicki <g.beznicki@gmail.com>
2019-12-12 22:53:02 +0100

    started again

--- WiredBrainCoffee/Models/MenuItem.cs
+++ WiredBrainCoffee/Models/MenuItem.cs
@@ -7,7 +7,6 @@ namespace WiredBrainCoffee.Models
 {
     public class MenuItem
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Summary { get; set; }
         public string Description { get; set; }
