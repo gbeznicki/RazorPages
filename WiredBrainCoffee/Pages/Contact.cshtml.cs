063a6682b3acd923e88d3cd0c6a99b79baa46ebf
gbeznicki <g.beznicki@gmail.com>
2019-12-21 18:35:15 +0100

    added Email service and succes email sent alert

--- WiredBrainCoffee/Pages/Contact.cshtml.cs
+++ WiredBrainCoffee/Pages/Contact.cshtml.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
 
 namespace WiredBrainCoffee.Pages
 {
     public class ContactModel : PageModel
     {
+        [BindProperty]
         public Contact Contact{ get; set; }
+        public string Message { get; private set; }
 
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            EmailService.SendEmail(Contact);
+            Message = "Your email has been sent.";
         }
     }
 }
